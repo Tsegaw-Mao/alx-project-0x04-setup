dcfbd6d8f2dec024af10c53f0379dc0fb7fff184
Tsegaw-Mao <tsegawjohnj@gmail.com>
2025-08-02 16:29:30 +0300

    Initaial commit

---
+++ alx-project-0x04/components/layouts/Layout.tsx
@@ -0,0 +1,18 @@
+//components/layouts/Layout
+
+import { LayoutProps } from "@/interface";
+import Footer from "./Footer";
+import Header from "./Header";
+
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
